70b544b1e76deac93aa3ca8283e1f5301ac3fa7e
Krystian Sulinski <krystian.sulinski@gmail.com>
2016-01-07 13:21:28 +1000

    update

--- plot/Experimental_Arduino_Monitor.py
+++ plot/Experimental_Arduino_Monitor.py
@@ -1,5 +1,5 @@
 import serial
-import threading
+from threading import Thread
 import time
 
 
@@ -13,8 +13,8 @@ class SerialData:
         # If no exception is raised during the try block, the else clause is executed afterwards.
         else:
             # create and start the thread's activity
-            threading.Thread(group=None, target=self.receiving(), args=self.serial).start()
-        self.number_of_plots = len(self.receiving())
+            Thread(group=None, target=self.receiving(), args=(self.serial,)).start()
+        self.number_of_plots = 4
 
     def receiving(self):
         while True:
@@ -24,36 +24,28 @@ class SerialData:
                 buffer_string = [int(i) for i in buffer_string.split()]
                 return buffer_string
 
-    def get(self, number_of_plot=None):
+    def get_next(self, number_of_plot):
         if not self.serial:
             print "Serial is not connected"
             return -1
 
-        if number_of_plot > self.number_of_plots:
+        if number_of_plot > self.number_of_plots or number_of_plot < 0:
             print "Demanding number_of_plot = {} is out of range. numbers_of_plot = {}".\
                 format(number_of_plot, self.number_of_plots)
             return -1
 
-        if number_of_plot is None:
-           # for i in range(40):
+        for i in range(40):
             try:
-                return self.receiving()
+                return self.receiving(self.serial)[number_of_plot]
             except ValueError:
                 print "No data"
-                time.sleep(.01)
-
-        # for i in range(40):
-        try:
-            return self.receiving()[number_of_plot]
-        except ValueError:
-            print "No data"
-            time.sleep(.01)
-
+                time.sleep(.005)
+        return -1
 
 if __name__ == "__main__":
     serial_data = SerialData()
     while True:
-        print serial_data.get(0)
+        print serial_data.get_next(0)
     # output:
     # [57, 124, 179, 205]
     # [58, 119, 171, 190]
